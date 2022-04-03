380975302aaa28860aedf8fa58dddf4957487501
Jeff Tranter <tranter@pobox.com>
2022-04-02 20:12:47 -0400

    Add code for writing to clock registers.

--- sbc/software/ds1216e.asm
+++ sbc/software/ds1216e.asm
@@ -25,7 +25,7 @@ BUFFER  equ     $2000           ; Buffer for clock data
 
         ldaa    ROM+4           ; Reset the sequence
         ldx     #pattern        ; Get pointer to pattern byte
-l2      clrb                    ; Shift count
+l2      clrb                    ; Clear shift count
         ldaa    0,x             ; Get byte of pattern
 l1      lsra                    ; Shift LSB into carry
         bcs     one
@@ -40,11 +40,11 @@ shift   incb                    ; Increment shift count
         bne     l2              ; If not, go back and continue
 
 ; Now read 64 register bits using 64 reads.
-; A2 must be high, data is in D0
+; A2 must be high, data is in D0.
 
         ldx     #BUFFER         ; Start of data buffer
 o2      clr     0,x             ; Initially clear byte
-        clrb                    ; Shift count
+        clrb                    ; Clear shift count
 o1      ldaa    ROM+4           ; Get data
         anda    #$01            ; Mask out all bits except D0
         asla                    ; Shift bit 0 into bit 7
@@ -63,6 +63,29 @@ o1      ldaa    ROM+4           ; Get data
         inx                     ; Increment pointer to next byte in pattern
         cpx     #BUFFER+8       ; Last byte reached?
         bne     o2              ; If not, go back and continue
+;       jmp     MONITOR         ; Done, back to monitor
+
+        ldaa    #$11            ; Need /RST bit to be 1 and /OSC to be 0.
+        staa    $2004
+
+; Now write 64 register bits using 64 reads.
+; A2 must be low, data is set by A0.
+
+        ldx     #BUFFER         ; Get pointer to data bytes
+m2      clrb                    ; Clear shift count
+        ldaa    0,x             ; Get byte of pattern
+m1      lsra                    ; Shift LSB into carry
+        bcs     wone
+        tst     ROM             ; Zero bit pattern
+        bra     shift2
+wone    tst     ROM+1           ; One bit pattern
+shift2  incb                    ; Increment shift count
+        cmpb    #8              ; Shifted 8 times?
+        bne     m1              ; If not, go back and continue
+        inx                     ; Increment pointer to next byte in pattern
+        cpx     #BUFFER+8       ; Last byte reached?
+        bne     m2              ; If not, go back and continue
+
         jmp     MONITOR         ; Done, back to monitor
 
 ; Enable pattern
